3d61dcfa3587f7f9f5957eb94c5a9b6d55c196c9
jjYBdx4IL <jjYBdx4IL@github.com>
2016-03-03 21:17:42 +0100

    add ExtendedGnuParser to allow ignoring unknown options

---
+++ src/main/java/com/github/jjYBdx4IL/utils/cli/ExtendedGnuParser.java
@@ -0,0 +1,50 @@
+/*
+ * #%L
+ * github-utils
+ * %%
+ * Copyright (C) 2016 jjYBdx4IL
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package com.github.jjYBdx4IL.utils.cli;
+
+import java.util.ListIterator;
+
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.ParseException;
+
+/**
+ *
+ * @author jjYBdx4IL
+ */
+public class ExtendedGnuParser extends GnuParser {
+
+    private boolean ignoreUnrecognizedOption;
+
+    public ExtendedGnuParser(final boolean ignoreUnrecognizedOption) {
+        this.ignoreUnrecognizedOption = ignoreUnrecognizedOption;
+    }
+
+    @Override
+    protected void processOption(final String arg, final ListIterator iter) throws ParseException {
+        boolean hasOption = getOptions().hasOption(arg);
+
+        if (hasOption || !ignoreUnrecognizedOption) {
+            super.processOption(arg, iter);
+        }
+    }
+
+}
