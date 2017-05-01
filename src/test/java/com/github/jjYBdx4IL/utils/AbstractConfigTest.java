5c7962503904f1f68420f90018ebcadff8b1b156
jjYBdx4IL <jjYBdx4IL@github.com>
2017-05-01 21:19:49 +0200

    fix license plugin errors

--- src/test/java/com/github/jjYBdx4IL/utils/AbstractConfigTest.java
+++ src/test/java/com/github/jjYBdx4IL/utils/AbstractConfigTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017 jjYBdx4IL (https://github.com/jjYBdx4IL)
+ * Copyright (C) 2016 jjYBdx4IL (https://github.com/jjYBdx4IL)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
